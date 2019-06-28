ba4ee1ffa7756e8393d684df593d26d80d50e4af
Tomas Vermilä <tomas.vermila@atea.fi>
2019-06-28 06:51:31 +0300

    8.2 tehty

--- index.js
+++ index.js
@@ -84,16 +84,25 @@ const books = [
 ]
 
 const typeDefs = gql`
+   type Book {
+    title: String!
+    published: Int!
+    author: String!
+    genres: [String!]!
+  }
+
   type Query {
     bookCount: Int!
-    authorCount: Int!
+    authorCount: Int!,
+    allBooks: [Book!]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 
