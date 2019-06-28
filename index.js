30f838eb902f74dcad8a4d8feb35e5369f9fb839
Tomas Vermilä <tvermila@gmail.com>
2019-06-28 07:10:01 +0300

    8.4 tehty

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -108,11 +108,11 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => books.filter(book => book.author === args.author),
     allAuthors: () => authors
   },
   Author: {
-    bookCount: (root) => books.filter(b => b.author === root.name).length
+    bookCount: (root) => books.filter(book => book.author === root.name).length
   }
 }
 
